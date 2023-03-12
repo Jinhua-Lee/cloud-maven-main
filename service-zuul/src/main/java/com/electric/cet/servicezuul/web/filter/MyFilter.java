f5d956077670419cab73ebca429fbb20d5728c90
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-03-12 15:01:54 +0800

    [chore] 一些版本调整.

--- service-zuul/src/main/java/com/electric/cet/servicezuul/web/filter/MyFilter.java
+++ service-zuul/src/main/java/com/electric/cet/servicezuul/web/filter/MyFilter.java
@@ -1,9 +1,7 @@
 package com.electric.cet.servicezuul.web.filter;
 
 import com.netflix.zuul.ZuulFilter;
-
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -19,10 +17,11 @@ public class MyFilter extends ZuulFilter {
 
     /**
      * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
-     *      pre：路由之前
-     *      routing：路由之时
-     *      post： 路由之后
-     *      error：发送错误调用
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     *
      * @return 过滤器类型
      */
     @Override
@@ -32,6 +31,7 @@ public class MyFilter extends ZuulFilter {
 
     /**
      * 过滤的顺序
+     *
      * @return 过滤顺序
      */
     @Override
@@ -41,30 +41,34 @@ public class MyFilter extends ZuulFilter {
 
     /**
      * 书写判断逻辑，是否过滤，
+     *
      * @return 是否执行过滤
      */
+    @Override
     public boolean shouldFilter() {
         return true;
     }
 
     /**
      * 过滤器的具体逻辑
-     * @return
-     * @throws ZuulException
+     *
+     * @return 返回对象
      */
-    public Object run() throws ZuulException {
+    @Override
+    public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
         System.out.println(request.getMethod());
         log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
         Object accessToken = request.getParameter("token");
-        if(accessToken == null) {
+        if (accessToken == null) {
             log.warn("token is empty");
             ctx.setSendZuulResponse(false);
             ctx.setResponseStatusCode(401);
             try {
                 ctx.getResponse().getWriter().write("token is empty");
-            } catch (Exception ignored){}
+            } catch (Exception ignored) {
+            }
 
             return null;
         }
