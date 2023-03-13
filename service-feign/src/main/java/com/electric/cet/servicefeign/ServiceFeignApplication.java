65cc4d3a3e7765a8a6970bf33f40c17f1368c336
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-03-13 13:29:39 +0800

    [style] 去掉无用的注解和类.

--- service-feign/src/main/java/com/electric/cet/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/electric/cet/servicefeign/ServiceFeignApplication.java
@@ -2,14 +2,14 @@ package com.electric.cet.servicefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * @author Jinhua-Lee
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 public class ServiceFeignApplication {
 
--- service-zuul/src/main/java/com/electric/cet/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/electric/cet/servicezuul/ServiceZuulApplication.java
@@ -2,19 +2,18 @@ package com.electric.cet.servicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Jinhua-Lee
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableZuulProxy
 public class ServiceZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceZuulApplication.class, args);
     }
-
 }
