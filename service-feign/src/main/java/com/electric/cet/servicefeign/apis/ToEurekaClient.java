19db28a7615e674953a711b573a96f9502e679ab
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-03-13 22:46:38 +0800

    [chore] 修正netflix组件版本.

--- service-feign/src/main/java/com/electric/cet/servicefeign/apis/ToEurekaClient.java
+++ service-feign/src/main/java/com/electric/cet/servicefeign/apis/ToEurekaClient.java
@@ -1,7 +1,7 @@
 package com.electric.cet.servicefeign.apis;
 
 import com.electric.cet.servicefeign.apis.impl.ToEurekaClientHystrix;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
