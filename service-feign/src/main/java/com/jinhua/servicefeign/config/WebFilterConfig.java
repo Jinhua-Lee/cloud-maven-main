f940116a15f032833a6db52ecae30d6bd2226740
Jinhua-Lee <lijinhua@cet-electric.com>
2023-12-06 17:07:47 +0800

    [feat] 增加路由日志.

---
+++ service-feign/src/main/java/com/jinhua/servicefeign/config/WebFilterConfig.java
@@ -0,0 +1,31 @@
+package com.jinhua.servicefeign.config;
+
+import com.jinhua.servicefeign.web.filter.RemoteMsgFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.Collections;
+
+/**
+ * @author Jinhua-Lee
+ */
+@Slf4j
+@Configuration
+public class WebFilterConfig {
+
+    @Value("${routing.filter-pattern:/*}")
+    private String routingFilterPattern;
+
+    @Bean
+    public FilterRegistrationBean<Filter> remoteMsgFilter() {
+        FilterRegistrationBean<Filter> remoteMsgFilterReg = new FilterRegistrationBean<>();
+        remoteMsgFilterReg.setFilter(new RemoteMsgFilter());
+        remoteMsgFilterReg.setOrder(0);
+        remoteMsgFilterReg.setUrlPatterns(Collections.singletonList(routingFilterPattern));
+        return remoteMsgFilterReg;
+    }
+}
