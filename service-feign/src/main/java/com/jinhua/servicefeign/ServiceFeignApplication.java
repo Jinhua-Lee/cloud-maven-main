47aa46423f0cee6a54da2f3557211210d199d716
Jinhua-Lee <lijinhua@cet-electric.com>
2024-10-14 14:33:56 +0800

    [revert] 去除功能：自定义feignClient的Url选择策略. [build] maven版本统一管理.

--- service-feign/src/main/java/com/jinhua/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/jinhua/servicefeign/ServiceFeignApplication.java
@@ -1,9 +1,7 @@
 package com.jinhua.servicefeign;
 
-import com.jinhua.servicefeign.config.ManualServiceListLoadBalancer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -13,7 +11,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@LoadBalancerClient(value = "eureka-client", configuration = ManualServiceListLoadBalancer.class)
 public class ServiceFeignApplication {
 
     public static void main(String[] args) {
