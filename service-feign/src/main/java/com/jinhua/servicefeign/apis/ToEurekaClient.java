73db11281ab918108b728c8bfc3143db3aded211
Jinhua-Lee <lijinhua@cet-electric.com>
2024-11-18 12:02:10 +0800

    [feat] ZuulFilter实现自定义路由功能，非微服务注册方式，路由到可用的url.修复自定义服务名的判断问题.

--- service-feign/src/main/java/com/jinhua/servicefeign/apis/ToEurekaClient.java
+++ service-feign/src/main/java/com/jinhua/servicefeign/apis/ToEurekaClient.java
@@ -2,6 +2,8 @@ package com.jinhua.servicefeign.apis;
 
 import com.jinhua.feigncommon.MyHelloDTO;
 import com.jinhua.servicefeign.apis.impl.ToEurekaClientHystrix;
+import com.jinhua.servicefeign.config.ManualServiceListLoadBalancer;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import java.util.List;
  * @author Jinhua-Lee
  */
 @FeignClient(value = "eureka-client", fallback = ToEurekaClientHystrix.class)
+@LoadBalancerClient(value = "eureka-client", configuration = ManualServiceListLoadBalancer.class)
 public interface ToEurekaClient {
 
     /**
--- service-zuul/src/main/java/com/electric/cet/servicezuul/config/ManualRoutingServerConfig.java
+++ service-zuul/src/main/java/com/electric/cet/servicezuul/config/ManualRoutingServerConfig.java
@@ -1,4 +1,4 @@
-package com.electric.cet.servicezuul;
+package com.electric.cet.servicezuul.config;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
