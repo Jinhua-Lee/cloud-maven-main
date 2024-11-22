cb54c245fc3e474a76ed95bbd1dc603fd3cdcfab
Jinhua-Lee <lijinhua@cet-electric.com>
2024-11-22 16:37:30 +0800

    [chore] ribbon负载均衡器，还没调通的先去除.

--- service-feign/src/main/java/com/jinhua/servicefeign/apis/ToEurekaClient.java
+++ service-feign/src/main/java/com/jinhua/servicefeign/apis/ToEurekaClient.java
@@ -2,13 +2,9 @@ package com.jinhua.servicefeign.apis;
 
 import com.jinhua.feigncommon.MyHelloDTO;
 import com.jinhua.servicefeign.apis.impl.ToEurekaClientHystrix;
-import com.jinhua.servicefeign.config.ManualServiceListLoadBalancer;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,7 +12,6 @@ import java.util.List;
  * @author Jinhua-Lee
  */
 @FeignClient(value = "eureka-client", fallback = ToEurekaClientHystrix.class)
-@LoadBalancerClient(value = "eureka-client", configuration = ManualServiceListLoadBalancer.class)
 public interface ToEurekaClient {
 
     /**
